929073e2de1e446febc11b100151f0609b3278d2
Jesse Mullan <jmullan@visi.com>
2013-11-29 19:03:57 -0800

    add second test from sample

--- tests/EngineTest.php
+++ tests/EngineTest.php
@@ -2,15 +2,9 @@
 namespace tests;
 
 
-/**
- *
- */
 class EngineTest extends \PHPUnit_Framework_TestCase
 {
 
-    /**
-     *
-     */
     public function testSampleOne()
     {
         $expected = file_get_contents(__DIR__ . '/sample1_output.htm');
@@ -25,7 +19,32 @@ class EngineTest extends \PHPUnit_Framework_TestCase
         $t->addBlock("block1");
         ob_start();
         $t->generateOutput();
-        $output = ob_get_clean();
-        $this->assertEquals($expected, $output);
+        $actual = ob_get_clean();
+        $this->assertEquals($expected, $actual);
+    }
+    public function testSampleTwo()
+    {
+        $t = new \MiniTemplator\Engine();
+        $ok = $t->readTemplateFromFile(__DIR__ . "/sample2_template.htm");
+        $t->setVariable("year", "2003");
+        $t->setVariable("month", "April");
+        for ($weekOfYear=14; $weekOfYear<=18; $weekOfYear++) {
+            for ($dayOfWeek=0; $dayOfWeek<7; $dayOfWeek++) {
+                $dayOfMonth =($weekOfYear*7 + $dayOfWeek) - 98;
+                if ($dayOfMonth >= 1 && $dayOfMonth <= 30) {
+                    $t->setVariable("dayOfMonth", $dayOfMonth);
+                } else {
+                    $t->setVariable("dayOfMonth", "&nbsp;");
+                }
+                $t->addBlock("day");
+            }
+            $t->setVariable("weekOfYear", $weekOfYear);
+            $t->addBlock("week");
+        }
+        ob_start();
+        $t->generateOutput();
+        $actual = ob_get_clean();
+        $expected = file_get_contents(__DIR__ . '/sample2_output.htm');
+        $this->assertEquals($expected, $actual);
     }
 }
